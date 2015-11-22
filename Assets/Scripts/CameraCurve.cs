75e0dc3f0d41a2adc48b02b6507d66172a8c83d4
Phil Lira <felipedrl@gmail.com>
2015-11-22 18:43:02 -0300

    Curve Shader Prototype added.

---
+++ Assets/Scripts/CameraCurve.cs
@@ -0,0 +1,102 @@
+﻿//The MIT License(MIT)
+
+//Copyright(c) 2015 Phil Lira
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraCurve : MonoBehaviour
+{
+    private GameObject[] _CurvedGameObjects;
+    private Vector3[][] _MeshVertices;
+    private const string CurvedGeometryTag = "CurvedGeometry";
+    public float _WorldLenght;
+
+    void Start()
+    {
+        _CurvedGameObjects = GameObject.FindGameObjectsWithTag(CurvedGeometryTag);
+        
+        // Creates a copy of all curved geometry vertices.
+        _MeshVertices = new Vector3[_CurvedGameObjects.Length][];
+        for (int i = 0; i < _CurvedGameObjects.Length; ++i)
+        {
+            Mesh mesh = _CurvedGameObjects[i].GetComponent<MeshFilter>().mesh;
+            _MeshVertices[i] = new Vector3[mesh.vertexCount];
+            for (int j = 0; j < mesh.vertexCount; ++j)
+            {
+                _MeshVertices[i][j] = mesh.vertices[j];
+            }
+        }
+    }
+
+    Vector3 RotateY(Vector3 v, float theta)
+    {
+        // rotation around up vector
+        // | cos(theta), 0, -sin(theta) | ( x )
+        // |     0     , 0,       0     | ( y )
+        // | sin(theta), 0,  cos(theta) | ( z )
+        float cosT = Mathf.Cos(theta);
+        float sinT = Mathf.Sin(theta);
+        return new Vector3(v.x * cosT - v.z * sinT, v.y, v.x * sinT + v.z * cosT);
+    }
+
+    Vector3 ApplyCurve(Vector3 vertex)
+    {
+        Vector3 camPos = transform.position;
+        float radius = vertex.z - camPos.z;
+        float s = vertex.x - camPos.x;
+
+        // Right hand system rotates yaw right as negative angle
+        float theta = s / radius;
+
+        return Quaternion.AngleAxis(theta * Mathf.Rad2Deg, Vector3.up) * vertex;
+    }
+
+    void OnPreCull()
+    {
+        for (int i = 0; i < _CurvedGameObjects.Length; ++i)
+        {
+            GameObject go = _CurvedGameObjects[i];
+            MeshFilter meshFilter = go.GetComponent<MeshFilter>();
+            if (meshFilter != null)
+            {
+                Mesh mesh = meshFilter.mesh;
+                Vector3[] vertices = mesh.vertices;
+
+                for (int j = 0; j < vertices.Length; ++j)
+                {
+                    // TODO: Instead of transforming each vertice to world space, perform bending computation on local space.
+                    vertices[j] = _CurvedGameObjects[i].transform.TransformPoint(_MeshVertices[i][j]);
+                    vertices[j] = ApplyCurve(vertices[j]);
+                    vertices[j] = _CurvedGameObjects[i].transform.InverseTransformPoint(vertices[j]);
+                }
+                mesh.vertices = vertices;
+            }
+        }
+    }
+
+    void DebugLine(Vector3 start, Vector3 end)
+    {
+#if DEBUG_RAYCAST
+        Debug.DrawLine(start, end, Color.red);
+#endif
+    }
+}
---
+++ Assets/Shaders/CurveShader.shader
@@ -0,0 +1,90 @@
+﻿Shader "Custom/CurveShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_BentStartPoint("BendRadius", Float) = 1.0
+	}
+	
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float3 _BendRadius;
+			
+			float3 RotateY(float3 v, float theta)
+			{
+				// rotation around up vector
+				// | cos(theta), 0, -sin(theta) | ( x )
+				// |     0     , 0,       0     | ( y )
+				// | sin(theta), 0,  cos(theta) | ( z )
+				float cosT = cos(theta);
+				float sinT = sin(theta);
+				return float3(v.x * cosT - v.z * sinT, v.y, v.x * sinT + v.z * cosT);
+			}
+
+			float3 ApplyCurve(float3 vertex)
+			{
+				float radius = vertex.z - _WorldSpaceCameraPos.z;
+				float3 startPointToCam = float3(_WorldSpaceCameraPos.x, _WorldSpaceCameraPos.y, radius);
+
+				float s = vertex.x - _WorldSpaceCameraPos.x;
+
+				// Right hand system rotates yaw right as negative angle
+				float theta = s / radius;
+
+				return RotateY(vertex.xyz, -theta);
+			}
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+								
+				float4 vertex = mul(_Object2World, v.vertex);
+				vertex.xyz = ApplyCurve(vertex.xyz);
+
+				o.vertex = mul(UNITY_MATRIX_VP, vertex);
+				
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o, o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);				
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
